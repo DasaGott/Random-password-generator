33c655b7ecf00bea942e48f18236fc9816f6db4f
DasaGott <142871686+DasaGott@users.noreply.github.com>
2024-01-03 20:46:02 +0100

    Defined variables, function for generating password

--- app.js
+++ app.js
@@ -1,3 +1,82 @@
+const resultEl =
+  document.getElementById("result");
+const lengthEl =
+  document.getElementById("length");
+const upperCaseEl =
+  document.getElementById("upperCase");
+const lowerCaseEl =
+  document.getElementById("lowerCase");
+const numberEl =
+  document.getElementById("numbers");
+const symbolsEl =
+  document.getElementById("symbols");
+const generateEl =
+  document.getElementById("generate");
+const clipboardEl =
+  document.getElementById("clipboard");
+
+generateEl.addEventListener("click", () => {
+  //plus sign interpolates string to the number
+  const length = +lengthEl.value;
+
+  //when checked, the return state is true, once the checkbox is not checed, return state is false
+  const hasUpper = upperCaseEl.checked;
+  const hasLower = lowerCaseEl.checked;
+  const hasNumber = numberEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasUpper,
+    hasLower,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+function generatePassword(
+  upper,
+  lower,
+  number,
+  symbol,
+  length
+) {
+  let generatedPassword = "";
+  const typesCount =
+    lower + upper + number + symbol;
+
+  const typesArr = [
+    { lower },
+    { upper },
+    { number },
+    { symbol },
+  ].filter((item) => Object.values(item)[0]);
+
+  if (typesCount === 0) {
+    return;
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(
+    0,
+    length
+  );
+  return finalPassword;
+}
+
 function getRandomLower() {
   return String.fromCharCode(
     Math.floor(Math.random() * 26) + 97
